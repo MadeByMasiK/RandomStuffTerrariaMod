c4612b0d3543c6d8a83af773a305e7e9a3b2ab9f
MadeByMasiK <59820807+MadeByMasiK@users.noreply.github.com>
2025-03-04 01:08:57 +0200

    created upgrade for sniper and giant sword

---
+++ Content/Items/ComicallyLargeSword.cs
@@ -0,0 +1,49 @@
+using RandomStuffByMasi.Content.Items.Materials;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Localization;
+using Terraria.GameContent.Creative;
+
+namespace RandomStuffByMasi.Content.Items
+{
+    public class ComicallyLargeSword : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 3; // How many items need for research in Journey Mode
+        }
+
+        public override void SetDefaults()
+        {
+            // Visual properties
+            Item.width = 54; // Width of an item sprite
+            Item.height = 62; // Height of an item sprite
+            Item.scale = 7.2f;
+
+            // Combat properties
+            Item.damage = 332;
+            Item.DamageType = DamageClass.Melee; // What type of damage item is deals, Melee, Ranged, Magic, Summon, Generic (takes bonuses from all damage multipliers), Default (doesn't take bonuses from any damage multipliers)
+            // useTime and useAnimation often use the same value, but we'll see examples where they don't use the same values
+            Item.useTime = 27; // How long the swing lasts in ticks (60 ticks = 1 second)
+            Item.useAnimation = 27; // How long the swing animation lasts in ticks (60 ticks = 1 second)
+            Item.knockBack = 20f; // How far the sword punches enemies, 20 is maximal value
+            Item.autoReuse = true; // Can the item auto swing by holding the attack button
+
+            // Other properties
+            Item.maxStack = 1; // How many items can be in one inventory slot
+            Item.value = 90; // Item sell price in copper coins
+            Item.rare = ItemRarityID.Yellow; // The color of item's name in game. Check https://terraria.wiki.gg/wiki/Rarity
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.UseSound = SoundID.Item1;}
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient<GiantSword>(1);
+            recipe.AddIngredient(ItemID.HallowedBar, 15);
+            recipe.AddTile(TileID.Anvils); // Crafting station we need for craft, WorkBenches, Anvils etc. You can find them here - https://terraria.wiki.gg/wiki/Tile_IDs
+            recipe.Register();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,15 +3,22 @@
 
 ## Added stuff
 
+<<<<<<< HEAD
 5 Weapons
+=======
+7 Weapons
+>>>>>>> 20b05d1 (created upgrade for sniper and giant sword)
 1 Material
 
 
 ## TODO
 
 Weapons:
+<<<<<<< HEAD
 - Shitsniper Upgrade
 - Giant Sword Upgrade (Literally giant)
+=======
+>>>>>>> 20b05d1 (created upgrade for sniper and giant sword)
 - Magic weapon
 - Summon weapon
 
